38df0a57d9c004cd8dd124a44518a7f9bd2e27c8
alnaseem <alnaseem@AHMED>
2022-07-09 05:49:45 -0800

    Add blood type

---
+++ HealthCare/HealthCare.API/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using HealthCare.API.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace HealthCare.API.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext>options):base(options)
+        {
+        }
+        public DbSet <BloodType> BloodTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<BloodType>().HasIndex(x => x.Description).IsUnique();    
+        }
+    }
+}
